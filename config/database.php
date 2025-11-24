0e7a74d2e53b69e8369c53acea27791f1ba9cdd4
ZaffService <seckmoustapha238@gmail.com>
2025-11-24 12:43:47 +0000

    Update MongoDB config default

--- config/database.php
+++ config/database.php
@@ -95,7 +95,7 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'dsn' => env('MONGODB_URI'),
+            'dsn' => env('MONGODB_URI', 'mongodb://localhost:27017'),
             'database' => env('MONGODB_DATABASE', 'om_pay_db'),
         ],
 
